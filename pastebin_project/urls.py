3c32420d25cafc69e1302a3c5315b36bfa74b82f
rodeatharva04 <123895899+rodeatharva04@users.noreply.github.com>
2026-01-27 17:48:17 +0530

    Update URL paths for paste fetching and creation

--- pastebin_project/urls.py
+++ pastebin_project/urls.py
@@ -4,7 +4,7 @@ from pastes import views
 urlpatterns = [
     path('api/healthz', views.healthz),
     path('api/pastes', views.create_paste),
-    path('api/pastes/<uuid:id>', views.fetch_paste_api),
-    path('p/<uuid:id>', views.fetch_paste_html),
-    path('', views.index),
+    path('api/pastes/<uuid:id>', views.fetch_api),
+    path('p/<uuid:id>', views.view_html),
+    path('', views.create_paste),
 ]
