476389747c38cd6df71c541430ea1e62985a9007
rodeatharva04 <123895899+rodeatharva04@users.noreply.github.com>
2026-01-27 18:24:28 +0530

    Update urls.py

--- pastebin_project/urls.py
+++ pastebin_project/urls.py
@@ -1,10 +1,16 @@
+from django.contrib import admin
 from django.urls import path
 from pastes import views
 
 urlpatterns = [
+    # Required API Routes
     path('api/healthz', views.healthz),
     path('api/pastes', views.create_paste),
     path('api/pastes/<uuid:id>', views.fetch_api),
+    
+    # Required HTML Route
     path('p/<uuid:id>', views.view_html),
+    
+    # Home (UI)
     path('', views.create_paste),
 ]
