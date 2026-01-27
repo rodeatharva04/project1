2946961b830b57aa66a090402256f6ef8892e157
rodeatharva04 <123895899+rodeatharva04@users.noreply.github.com>
2026-01-27 17:56:43 +0530

    Update settings.py

--- pastebin_project/settings.py
+++ pastebin_project/settings.py
@@ -36,11 +36,12 @@ ROOT_URLCONF = "pastebin_project.urls"
 WSGI_APPLICATION = "pastebin_project.wsgi.application"
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
-        conn_max_age=600
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 TEMPLATES = [
     {
